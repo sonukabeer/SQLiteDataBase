88b19640e4ed1f258cb93b0c2eaabb810d9b010e
sonu yadav <44773393+sonukabeer@users.noreply.github.com>
2021-08-13 03:31:33 +0530

    SQLiteDataBase

---
+++ app/src/main/java/com/sonukumariit8/sqlitedb/MainActivity.java
@@ -0,0 +1,18 @@
+package com.sonukumariit8.sqlitedb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        DbHandler handler=new DbHandler(this,"empdb",null,1);
+      //  handler.addEmployee(new Employee(10,"kabeer",33.3));
+        handler.getEmployee(1);
+        handler.close();
+    }
+}
